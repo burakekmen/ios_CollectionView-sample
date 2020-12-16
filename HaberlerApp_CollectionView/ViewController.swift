a73c2147f7e384aea7672c27f6c0c08d9bd9bf02
Burak Ekmen <ekmen.burak@hotmail.com>
2020-12-16 15:33:43 +0300

    Initial Commit

---
+++ HaberlerApp_CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HaberlerApp_CollectionView
+//
+//  Created by BURAK EKMEN on 16.12.2020.
+//  Copyright © 2020 Burak Ekmen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
